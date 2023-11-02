76101e1069a6c236ede4fda5aaa3c5da31fca630
Juani Campora <juanicampora@gmail.com>
2023-11-02 03:47:43 -0300

    Greeting agregada

--- src/views/InicioView.vue
+++ src/views/InicioView.vue
@@ -2,5 +2,10 @@
 </script>
 
 <template>
-  <h1>Inicio </h1>
+  <div class="container py-4 rounded mt-3" style="background-color: darkgray;">
+    <div>
+      <h1 class="text-center display-5 fw-bold text-body-emphasis mb-3">¡Bienvenido a FiestApp!</h1>
+
+    </div>
+  </div>
 </template>
