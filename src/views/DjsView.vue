6e585406d4cc31f151bd3854c75dc7bc55901a7d
Juani Campora <juanicampora@gmail.com>
2023-11-29 21:03:15 -0300

    Agregados

--- src/views/DjsView.vue
+++ src/views/DjsView.vue
@@ -4,12 +4,17 @@ import EditarDJ from '@/components/EditarDJ.vue';
 import OpinionesDJ from '@/components/OpinionesDJ.vue';
 import axios from 'axios';
 import moment from 'moment';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
 
 const djs = ref([]);
 const estadoEditor = ref(false);
 const djEditar = ref(0);
 const estadoOpiniones = ref(false);
 const djOpiniones = ref(0);
+const esperandoAPI = ref(false);
+const fullPage = ref(true);
+
 
 const activarEditor = (djTabla) => {
   estadoEditor.value = false;
@@ -50,7 +55,10 @@ const hacerActual = (djTabla) => {
 
 const getData = async () => {
   try {
+    esperandoAPI.value = true;
     const { data } = await axios.get('https://fiestaappapi.onrender.com/api/djs');
+    console.log("PRUEBAAAA")
+    esperandoAPI.value = false;
     djs.value = data.data;
   } catch (error) {
     console.log(error)
@@ -62,42 +70,45 @@ getData();
 
 <template>
   <div>
-    <EditarDJ v-if="estadoEditor" :djEditar="djEditar" @cerrarEditor="estadoEditor = false" @getData="getData" />
-    <OpinionesDJ v-if="estadoOpiniones" :djOpiniones="djOpiniones" @cerrarOpiniones="estadoOpiniones = false" />
-  </div>
-  <div class="container py-4 rounded mt-3" style="background-color: darkgray;">
+    <loading v-model:active="esperandoAPI" color="#42b883" :is-full-page="fullPage" />
     <div>
-      <h1 class="text-center display-5 fw-bold text-body-emphasis">DJs</h1>
-      <h6 class="text-center">(DJ ACTUAL en verde)</h6>
-      <div class="mx-4">
-        <div class="my-3">
-          <button type="button" class="btn btn-success" @click="nuevoDJ" style="width: 300px; font-size: large;">Nuevo
-            DJ</button>
+      <EditarDJ v-if="estadoEditor" :djEditar="djEditar" @cerrarEditor="estadoEditor = false" @getData="getData" />
+      <OpinionesDJ v-if="estadoOpiniones" :djOpiniones="djOpiniones" @cerrarOpiniones="estadoOpiniones = false" />
+    </div>
+    <div class="container py-4 rounded mt-3" style="background-color: darkgray;">
+      <div>
+        <h1 class="text-center display-5 fw-bold text-body-emphasis">DJs</h1>
+        <h6 class="text-center">(DJ ACTUAL en verde)</h6>
+        <div class="mx-4">
+          <div class="my-3">
+            <button type="button" class="btn btn-success" @click="nuevoDJ" style="width: 300px; font-size: large;">Nuevo
+              DJ</button>
+          </div>
+          <table class="table">
+            <thead>
+              <th>Nombre</th>
+              <th>Instagram</th>
+              <th>Celular</th>
+              <th>Ultima fecha que tocó</th>
+              <th class="d-flex justify-content-end">Accion</th>
+            </thead>
+            <tbody>
+              <tr v-for="dj in  djs " :key="dj.id" :class="{ 'table-success': dj.actual }">
+                <td>{{ dj.nombre }}</td>
+                <td>{{ dj.instagram }}</td>
+                <td>{{ dj.tel }}</td>
+                <td>{{ dj.fechaActual }}</td>
+                <td class=" d-flex justify-content-end">
+                  <div class="btn-group">
+                    <button class="btn btn-info" @click="hacerActual(dj)" :disabled="dj.actual">Hacer Actual</button>
+                    <button class="btn btn-warning" @click="activarEditor(dj)">Editar</button>
+                    <button class="btn btn-secondary" @click="verOpiniones(dj)">Ver Opiniones</button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <table class="table">
-          <thead>
-            <th>Nombre</th>
-            <th>Instagram</th>
-            <th>Celular</th>
-            <th>Ultima fecha que tocó</th>
-            <th class="d-flex justify-content-end">Accion</th>
-          </thead>
-          <tbody>
-            <tr v-for="dj in  djs " :key="dj.id" :class="{ 'table-success': dj.actual }">
-              <td>{{ dj.nombre }}</td>
-              <td>{{ dj.instagram }}</td>
-              <td>{{ dj.tel }}</td>
-              <td>{{ dj.fechaActual }}</td>
-              <td class=" d-flex justify-content-end">
-                <div class="btn-group">
-                  <button class="btn btn-info" @click="hacerActual(dj)" :disabled="dj.actual">Hacer Actual</button>
-                  <button class="btn btn-warning" @click="activarEditor(dj)">Editar</button>
-                  <button class="btn btn-secondary" @click="verOpiniones(dj)">Ver Opiniones</button>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     </div>
   </div>
